5b7cd38a553ded3efff9d9842e9d5253e575c764
Michael Herman <hermanmu@gmail.com>
2015-03-03 10:28:57 -0700

    updates

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,5 @@
 
 curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/mjhea0/node-docker-workflow.git
-cd node-docker-workflow
 docker-compose build
 docker-compose up -d
