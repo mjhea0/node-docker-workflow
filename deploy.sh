06de006e730bd8b698d0d3d87ded99c7e26715d1
Michael Herman <hermanmu@gmail.com>
2015-03-03 10:25:41 -0700

    added deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@
 
 curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-docker-compose up
+git clone https://github.com/mjhea0/node-docker-workflow.git
+cd node-docker-workflow
+docker-compose build
+docker-compose up -d
